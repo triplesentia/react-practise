e1c41f28b8120d190abba60643baefba41611309
Artemiy K. <triplesentia@gmail.com>
2024-08-03 12:47:49 +0300

    add toggle btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
 import './App.css';
-
+import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const {tg, onToggleButton} = useTelegram();
 
   useEffect (() => {
     tg.ready();
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      work
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
